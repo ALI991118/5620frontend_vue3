d7660244fa572bc5c1de5e90a127f12cf92d9697
ALI991118 <2906175408@qq.com>
2022-10-25 09:45:24 +0800

    build project

---
+++ project5620_frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+</script>
+
+<template>
+  <el-row class="mb-4">
+    <el-button disabled>Default</el-button>
+    <el-button type="primary" disabled>Primary</el-button>
+    <el-button type="success" disabled>Success</el-button>
+    <el-button type="info" disabled>Info</el-button>
+    <el-button type="warning" disabled>Warning</el-button>
+    <el-button type="danger" disabled>Danger</el-button>
+  </el-row>
+
+
+</template>
