ce73279030d66f71f03c2a7ba8fadd483946f184
ALI991118 <2906175408@qq.com>
2022-10-25 16:52:12 +0800

    exhibit page template

--- project5620_frontend/src/router/index.js
+++ project5620_frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/LoginPage.vue'
 import RegisterPage from '../views/RegisterPage.vue'
+import MainPage from '../views/ExhibitPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,10 +15,11 @@ const router = createRouter({
       path: '/RegisterPage',
       name: 'Register',
       component: RegisterPage
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/ExhibitPage',
+      name: 'ExhibitPage',
+      component: MainPage
     }
   ]
 })
---
+++ project5620_frontend/src/views/ExhibitPage.vue
@@ -0,0 +1,64 @@
+<template>
+    
+    <el-row>
+        <!-- <div>
+        exhibits
+    </div> -->
+      <el-col class="card"
+        v-for="(o, index) in 2"
+        :key="o"
+        :span="10"
+        :offset="index > 0 ? 2 : 0"
+      >
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="src/assets/exhibit01.jpg"
+            class="image"
+          />
+          <div style="padding: 14px">
+            <span>Woman with a Parasol</span>
+            <div class="bottom">
+              <!-- <time class="time">{{ currentDate }}</time> -->
+              <el-button text class="button">Operating</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </template>
+  
+  <script lang="ts" setup>
+  import { ref } from 'vue'
+  
+  const currentDate = ref(new Date())
+  </script>
+  
+  <style>
+  <!-- .card{
+    width:250px;
+    top:20%;
+  } -->
+  .time {
+    font-size: 12px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .button {
+    padding: 0;
+    min-height: auto;
+  }
+  
+  .image {
+    width: 100%;
+    display: block;
+  }
+  </style>
+  
